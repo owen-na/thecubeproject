6ea4bdcbf4a20e2f4228e24310d3f474cce686b8
owen <osn53387@uga.edu>
2023-05-20 17:30:27 -0400

    began backend, fixed some auth pathing

--- components/top/top.tsx
+++ components/top/top.tsx
@@ -2,21 +2,41 @@ import styles from '@styles/top.module.css'
 import Image from 'next/image'
 import MyGif from '../../assets/215984.gif'
 import Link from 'next/link';
+import { useSession } from 'next-auth/react';
 
 // This function will return the users name on load if there are logged in with cookies
 // With a welcome back, ________
 // login and sighnup will be based on github creds. 
 // Next.authjs prob
-function Authinticated() {
+function Authenticated() {
+    const { data: session, status } = useSession();
+
     
+  if (status === 'loading') {
+    return <p>Loading screen here/splash page</p>;
+  }
+    if (!session) {
+        return (
+            <>
+            <p>not authd, not sure what to do here</p>
+            </>
+        )
+    }
+
+    return (
+        <>
+      <p>Welcome back, {session.user!.name}!</p>
+    </>
+  );
 }
 
 const imageStyle = {
     borderRadius: '25%',
-
 };
 
 export default function top() {
+    const { data: session, status } = useSession!();
+
     return (
         <div className={styles.header}>
             <Image
@@ -30,12 +50,19 @@ export default function top() {
                 <button>Access User Base</button>
             </div>
             <div className={styles.loginstatus}>
+                {status === 'loading' && <p>Loading...</p>}
+        {session ? (
+          <p>Welcome back, {session.user!.name}!</p>
+        ) : (
+            <>
                 <Link href="/login">
                     <button className={styles.loginButton}>Login</button>
                 </Link>
                 <Link href="/login">
                 <button className={styles.loginButton}>Sign Up</button>
                 </Link>
+                </>
+        )}
             </div>
         </div>
     )
