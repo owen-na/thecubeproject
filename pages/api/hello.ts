5d6239e86df74db8ca7a20de46ed662b3ff2b88b
owen <osn53387@uga.edu>
2023-05-21 15:24:08 -0400

    continued on backend development

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+// import { Pool } from '../../tcp-be/server'
 
 type Data = {
   name: string
@@ -7,7 +8,19 @@ type Data = {
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse // <Data>, might need it, no idea yet
 ) {
+  const { method } = req;
+  console.log(method)
+
+  // need to figure out how to have like expresses app.post(location), how do i address the location here????
+
+  switch(method) {
+    case 'POST':
+        res.status(200).json({response: "HELLLO"})
+    case 'GET':
+        res.status(200).json({response: "THIS SHOULD BE A GETTER??"})
+  }
+
   res.status(200).json({ name: 'John Doe' })
 }
--- tcp-be/server.ts
+++ tcp-be/server.ts
@@ -1,1 +1,10 @@
 import { Pool } from 'pg'; // refer to stockbets if needed
+
+const pool = new Pool({  
+    connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+module.exports = { pool };
